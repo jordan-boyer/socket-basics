7ea13a9616afdac92df04e2746a3802f583e5fdf
Jordan Boyer <jordan.boyer@epitech.eu>
2016-01-04 12:09:29 +0100

    Add @currentUsers

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ var clientInfo = {};
 
 app.use(express.static(__dirname + "/public"));
 
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === "undifined") {
+		return ;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit("message", {
+		name: "System" ,
+		text: "Current users: " + users.join(", "),
+		timestamp: moment().valueOf()
+	});
+}
+
 io.on("connection", function(socket) {
 	console.log("User connected via socket.io!");
 
@@ -37,8 +60,13 @@ io.on("connection", function(socket) {
 
 	socket.on("message", function (message) {
 		console.log("Message received:" + message.text);
-		message.timestamp = moment().valueOf();
+
+		if (message.text === "@currentUsers") {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
 		io.to(clientInfo[socket.id].room).emit("message", message);
+		}
 	});
 
 	socket.emit("message", {
