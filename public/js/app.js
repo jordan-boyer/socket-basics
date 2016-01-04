fb802bed34394310b8dbe2d513f3bf664bd955d1
Jordan Boyer <jordan.boyer@epitech.eu>
2016-01-04 11:30:21 +0100

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,21 @@ var name = getQueryVariable("name") || "Anonymous";
 var room = getQueryVariable("room");
 var socket = io();
 
+jQuery(".room-title").text(room);
+
 socket.on("connect", function () {
 	console.log("connected to socket.io server!");
+	socket.emit("joinRoom", {
+		name: name,
+		room: room
+	});
 });
 
 socket.on("message", function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
-	
 
 	var $message = jQuery(".messages");
+
 
 	$message.append("<p><strong>" + message.name + " " + momentTimestamp.local().format("h:mm a:") + "</strong></p>");
 	$message.append("<p>" + message.text + "<p>");
