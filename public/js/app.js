b894283a2cdfb650f44df7b3bbaa1ee5e1c5e992
Jordan Boyer <jordan.boyer@epitech.eu>
2016-01-04 18:26:08 +0100

    Add style for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -15,11 +15,13 @@ socket.on("connect", function () {
 socket.on("message", function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 
-	var $message = jQuery(".messages");
+	var $messages = jQuery(".messages");
+	var $message = jQuery('<li class="list-group-item"></li>');
 
 
 	$message.append("<p><strong>" + message.name + " " + momentTimestamp.local().format("h:mm a:") + "</strong></p>");
 	$message.append("<p>" + message.text + "<p>");
+	$messages.append($message);
 });
 
 var $form = jQuery("#message-form");
